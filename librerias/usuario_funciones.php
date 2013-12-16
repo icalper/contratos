b64a6041fcc6dc0699ed66ca1df6797ce307878b
icalper <icalper@AcerAspire4552>
2013-12-16 07:07:43 -0600

    Teerminado panel de usuarios para el admin supremo. Calidad de codigo: Hecho en china, diseñado en la camara de diputados de mexico con un presupuesto de 2 chicles motita y un bubu-lubu.

--- librerias/usuario_funciones.php
+++ librerias/usuario_funciones.php
@@ -1,6 +1,7 @@
 <?php
 
 $datos = array();
+require 'librerias/db.php';
 
 
 //$usuario, $nombre, $password, $depto, $privilegios
@@ -12,23 +13,64 @@ switch ($accion) {
         $password = filter_input(INPUT_POST, "password");
         $depto = filter_input(INPUT_POST, "depto");
         $privilegios = filter_input(INPUT_POST, "privilegios");
-        if($usuario == "" || $nombre == "" || $password == "" || $depto == "" || $privilegios == ""){
-            $_SESSION['msg']= array("blue","Hay uno o más campos vacíos");
+        if ($usuario == "" || $nombre == "" || $password == "" || $depto == "" || $privilegios == "") {
+            $_SESSION['msg'] = array("blue", "Hay uno o más campos vacíos");
             break;
         }
         agregar($usuario, $nombre, $password, $depto, $privilegios);
         break;
     case "modificar":
+        $idusuario = filter_input(INPUT_POST, "idUsuario");
+        $usuario = filter_input(INPUT_POST, "usuario");
+        $nombre = filter_input(INPUT_POST, "nombre");
+        $password = filter_input(INPUT_POST, "password");
+        $depto = filter_input(INPUT_POST, "depto");
+        $privilegios = filter_input(INPUT_POST, "privilegios");
+        if($password == ""){
+            $modificar_sql = "UPDATE `contratos`.`usuario`
+            SET
+            `tipoUsuario` = '$privilegios',
+            `depUsuario` = '$depto',
+            `nomPersonaUsuario` = '$nombre'
+        WHERE `idUsuario` = $idusuario;";
+        }else{
+            $modificar_sql = "UPDATE `contratos`.`usuario`
+            SET
+            `tipoUsuario` = '$privilegios',
+            `depUsuario` = '$depto',
+            `contraseña` = '$password',
+            `nomPersonaUsuario` = '$nombre'
+            WHERE `idUsuario` = $idusuario;";
+        }
+//        die($modificar_sql);
+        if (mysql_query($modificar_sql)) {
+            $_SESSION['msg'] = array("green", "El usuario ha sido modificado");
+        } else {
+            $_SESSION['msg'] = array("red", "El usuario NO ha sido modificado: ".  mysql_error());
+        }
         break;
     case "eliminar":
+        $idusuario = filter_input(INPUT_POST, "idUsuario");
+        $eliminar_sql = "DELETE FROM contratos.usuario WHERE idUsuario = $idusuario";
+        if (mysql_query($eliminar_sql)) {
+            $_SESSION['msg'] = array("green", "El usuario ha sido eliminado");
+        } else {
+            $_SESSION['msg'] = array("red", "El usuario NO ha sido eliminado");
+        }
         break;
-    case "eliminar_definitivo":
+    case "asignar":
+        $idusuario = filter_input(INPUT_POST, "idUsuario");
+        $numcontrato = filter_input(INPUT_POST, "numContrato");
+        $asignar_sql = "INSERT INTO contratos.asignacion (idAsignacion, idUsuario, idContrato) "
+            . "         VALUES (null, $idusuario, (SELECT idContrato FROM contratos.contrato WHERE numContrato = '$numcontrato'));";
+        mysql_query($asignar_sql);
+        header("Location: usuario.php?f=as&u=$idusuario");
+        
         break;
     default :
 }
 
 function agregar($usuario, $nombre, $password, $depto, $privilegios) {
-    require 'librerias/db.php';
     $pass_md5 = md5($password);
     $agregar_sql = "INSERT INTO `contratos`.`usuario`(`idUsuario`, `nomUsuario`, `tipoUsuario`, `depUsuario`, `contraseña`, `nomPersonaUsuario`)
                     VALUES
@@ -37,15 +79,70 @@ function agregar($usuario, $nombre, $password, $depto, $privilegios) {
         $error = mysql_errno();
         $msg = array("red", $error . " : " . mysql_error());
     } else {
-        $msg = array("green","Usuario " . $usuario . " ha sido registrado");
+        $msg = array("green", "Usuario " . $usuario . " ha sido registrado");
     }
-    $_SESSION['msg']=$msg;
+    $_SESSION['msg'] = $msg;
 }
 
 function modificar() {
     
 }
 
-function eliminar() {
+function getUsuario($idUsuario) {
+    $getUsuario_sql = "SELECT idUsuario, nomUsuario, nomPersonaUsuario, tipoUsuario, depUsuario FROM contratos.usuario WHERE idUsuario = '$idUsuario'";
+    $result = mysql_query($getUsuario_sql);
+    $usuario = mysql_fetch_row($result, MYSQL_ASSOC);
+
+    if (!$result) {
+        $error = mysql_errno();
+        $msg = array("red", $error . " : " . mysql_error());
+    }
+    $_SESSION['msg'] = $msg;
+
+    return $usuario;
+}
+
+function getUsuarios() {
+    $usuarios = array();
+    $getUsuarios_sql = "SELECT idUsuario, nomUsuario, tipoUsuario, nomPersonaUsuario FROM contratos.usuario WHERE tipoUsuario < 4";
+    $result = mysql_query($getUsuarios_sql);
+
+    if (!$result) {
+        $error = mysql_errno();
+        $msg = array("red", $error . " : " . mysql_error());
+        $_SESSION['msg'] = $msg;
+    }
+
+
+    while ($registro = mysql_fetch_array($result, MYSQL_BOTH)) {
+        array_push($usuarios, $registro);
+    }
+    return $usuarios;
+}
+
+function getContratos($idUsuario){
+    $contratos = array();
+    $getContratos_sql = "SELECT idAsignacion, numContrato, tipo
+                        FROM contratos.asignacion JOIN contratos.contrato 
+                        ON asignacion.idContrato = contrato.idContrato 
+                        WHERE idUsuario = $idUsuario;";
+    $result = mysql_query($getContratos_sql);
     
+    if (!$result) {
+        $error = mysql_errno();
+        $msg = array("red", $error . " : " . mysql_error());
+        $_SESSION['msg'] = $msg;
+    }
+    
+    while ($registro = mysql_fetch_array($result, MYSQL_BOTH)) {
+        array_push($contratos, $registro);
+    }
+    return $contratos;
+    
+}
+
+function eliminarContrato($idAsignacion) {
+    $eliminarContrato_sql = "DELETE FROM contratos.asignacion WHERE idAsignacion = $idAsignacion";
+    
+    mysql_query($eliminarContrato_sql);
 }
--- usuario.php
+++ usuario.php
@@ -4,7 +4,7 @@ $nivelAcceso = manejadorSesion::USUARIO_SUPERADMIN;
 $sesion = new manejadorSesion;
 
 
-$tipo = filter_input(INPUT_GET, "a");
+$formulario = filter_input(INPUT_GET, "f");
 $accion = filter_input(INPUT_POST, "accion");
 include './librerias/usuario_funciones.php';
 if ($sesion->getPrivilegios() < $nivelAcceso) {
@@ -42,8 +42,10 @@ if ($sesion->getPrivilegios() < $nivelAcceso) {
             </header>
             <div id="site_content">
                 <div id="content">
-
-
+                    <?php 
+                    switch($formulario){        // Agregar usuario
+                         case "a": ?>
+                           
                     <h1>Agregar usuario</h1>
                     <h2 style="color: <?php echo $_SESSION['msg'][0]; ?>">
                         <?php echo $_SESSION['msg'][1]; unset($_SESSION['msg'])?></h2>
@@ -72,60 +74,150 @@ if ($sesion->getPrivilegios() < $nivelAcceso) {
                             <p style="padding-top: 15px"><span>&nbsp;</span><input class="submit" type="submit" value="Agregar"/>
                         </div>
                     </form>
-
+                    
+                    <?php  break;               // Modificar usuarios
+                       case "m": ?>
+                    
                     <h1>Modificar usuario</h1>
+                    <?php $usuario = getUsuario(filter_input(INPUT_GET, "u"))?>
+                    <h2 style="color: <?php echo $_SESSION['msg'][0]; ?>">
+                        <?php echo $_SESSION['msg'][1]; unset($_SESSION['msg'])?></h2>
                     <form action="usuario.php" method="POST">
                         <div class="form_settings">
                             <input type="hidden" name="accion" value="modificar">
+                            <input type="hidden" name="idUsuario" value="<?php echo $usuario['idUsuario']?>">
                             <p><span>Usuario:</span>
-                                <input class="contact" disabled type="text" name="usuario"/></p>
+                                <input class="contact" disabled type="text" name="usuario" value="<?php echo $usuario['nomUsuario']?>"/></p>
 
                             <p><span>Nombre:</span>
-                                <input class="contact" type="text" name="nombre"/></p>
+                                <input class="contact" type="text" name="nombre" value="<?php echo $usuario['nomPersonaUsuario']?>"/></p>
 
                             <p><span>Contraseña:</span>
                                 <input class="contact" type="password" name="password"/></p>
 
                             <p><span>Departamento:</span>
-                                <input class="contact" type="text" name="depto"/></p>
+                                <input class="contact" type="text" name="depto" value="<?php echo $usuario['depUsuario']?>"/></p>
 
                             <p><span>Nivel de acceso:</span>
                                 <select size="3" name="privilegios">
-                                    <option value="1">Creador de contratos</option>
-                                    <option value="2">Supervisor de contratos(s)</option>
-                                    <option value="3">Administrador</option>
+                                    <option <?php  if($usuario['tipoUsuario'] == 1) {echo 'selected';}?> value="1">Creador de contratos</option>
+                                    <option <?php  if($usuario['tipoUsuario'] == 2) {echo 'selected';}?> value="2">Supervisor de contratos(s)</option>
+                                    <option <?php  if($usuario['tipoUsuario'] == 3) {echo 'selected';}?> value="3">Administrador</option>
                                 </select>
                             </p>
                             <p style="padding-top: 15px"><span>&nbsp;</span><input class="submit" type="submit" value="Modificar"/>
                         </div>
                     </form>
 
+                    <?php  break;               // Eliminacion de usuarios
+                       case "e": ?>
+                    
                     <h1>Eliminar usuario</h1>
+                     <?php $usuario = getUsuario(filter_input(INPUT_GET, "u"))?>
+                    <h2 style="color: <?php echo $_SESSION['msg'][0]; ?>">
+                        <?php echo $_SESSION['msg'][1]; unset($_SESSION['msg'])?></h2>
                     <h2 style="color: red">¡ATENCION! Confirme que desea eliminar el siguiente usuario:</h2>
                     <form action="usuario.php" method="POST">
                         <div class="form_settings">
-                            <input type="hidden" name="accion" value="eliminar_definitivo">
+                            <input type="hidden" name="accion" value="eliminar">
+                            <input type="hidden" name="idUsuario" value="<?php echo $usuario['idUsuario']?>">
                             <p><span>Usuario:</span>
-                                <input class="contact" disabled type="text" name="usuario"/></p>
+                                <input class="contact" disabled type="text" name="usuario" value="<?php echo $usuario['nomUsuario']?>"/></p>
 
                             <p><span>Nombre:</span>
-                                <input class="contact" disabled type="text" name="nombre"/></p>
+                                <input class="contact" disabled type="text" name="nombre" value="<?php echo $usuario['nomPersonaUsuario']?>"/></p>
 
                             <p><span>Departamento:</span>
-                                <input class="contact" disabled type="text" name="depto"/></p>
+                                <input class="contact" disabled type="text" name="depto" value="<?php echo $usuario['depUsuario']?>"/></p>
 
                             <p><span>Nivel de acceso:</span>
                                 <select disabled name="privilegios">
-                                    <option  value="1">Creador de contratos</option>
-                                    <option value="2">Supervisor de contratos(s)</option>
-                                    <option value="3">Administrador</option>
+                                    
+                                    <option <?php  if($usuario['tipoUsuario'] == 1) {echo 'selected';}?> value="1">Creador de contratos</option>
+                                    <option <?php  if($usuario['tipoUsuario'] == 2) {echo 'selected';}?> value="2">Supervisor de contratos(s)</option>
+                                    <option <?php  if($usuario['tipoUsuario'] == 3) {echo 'selected';}?> value="3">Administrador</option>
                                 </select>
                             </p>
                             <p style="padding-top: 15px"><span>&nbsp;</span><input class="submit" type="submit" value="Eliminar"/>
                         </div>
                     </form>
-
-
+                    
+                    <?php  break;              // Asignaciones
+                       case "as": ?>               
+                        <?php 
+                        $contratos_nombres = array(
+                            co => "Contrato de Obra",
+                            cs => "Contrato de Servicio",
+                            cci => "Contrato con Instalación",
+                            aa => "Acuerdo Administrativo"
+                        );
+                        $contrato_eliminar = filter_input(INPUT_GET, "c");
+                        if($contrato_eliminar != ""){
+                            eliminarContrato($contrato_eliminar);
+                        }
+                        $usuario = getUsuario(filter_input(INPUT_GET, "u"));
+                        $contratos = getContratos(filter_input(INPUT_GET, "u"));
+                        ?>
+                        <h1>Asignaciones para <?php echo $usuario['nomUsuario']?></h1>
+                        <h2 style="color: <?php echo $_SESSION['msg'][0]; ?>">
+                        <?php echo $_SESSION['msg'][1]; unset($_SESSION['msg'])?></h2>
+                        <form action="usuario.php" method="POST">
+                        <div class="form_settings">
+                            <input type="hidden" name="accion" value="asignar">
+                            <input type="hidden" name="idUsuario" value="<?php echo $usuario['idUsuario']?>">
+                            <p><span>Contrato:</span>
+                            <input class="contact" type="text" name="numContrato"/></p>
+                        <p style="padding-top: 15px"><span>&nbsp;</span><input class="submit" type="submit" value="Asignar"/>
+                        </div>
+                        </form>
+                        
+                        <table>
+                        <thead><th>Contrato</th><th>Tipo</th><th>Accion</th></thead>
+                        <tbody>
+                            <?php
+                            foreach ($contratos as $contrato) {
+                            echo "<tr><td>".$contrato['numContrato']."</td><td>".
+                            $contratos_nombres[strtolower($contrato['tipo'])]."</td><td>"
+                                    . "<a href=\"usuario.php?f=as&u=".$usuario['idUsuario'].
+                                    "&c=".$contrato['idAsignacion']."\">Eliminar asignacion</a>"
+                                    . "</td></tr>";
+                        }
+                        ?>
+                        </tbody>
+                        </table>
+                        
+                    <?php  break;               // Lista de usuarios
+                        default : ?>
+                    
+                    <h1>Lista de usuarios</h1>
+                    <?php $usuarios = getUsuarios();?>
+                    <h2 style="color: <?php echo $_SESSION['msg'][0]; ?>">
+                        <?php echo $_SESSION['msg'][1]; unset($_SESSION['msg'])?></h2>
+                    <table>
+                        <thead><th>Usuario</th><th>Nombre</th><th>Nivel</th><th>Acciones</th></thead>
+                    <tbody>
+                        <?php 
+                        $privilegios_nombres = array("", "Creador de contratos", "Supervisor de contratos", "Administrador");
+                        foreach ($usuarios as $usuario) {
+                            echo "<tr><td>".$usuario['nomUsuario']."</td><td>".
+                                    $usuario['nomPersonaUsuario']."</td><td>".
+                                    $privilegios_nombres[$usuario['tipoUsuario']]."</td><td>";
+                            if($usuario['tipoUsuario'] == 2){
+                                echo "<a href=\"usuario.php?f=as&u=".
+                                    $usuario['idUsuario']."\">Asignaciones</a> - ";
+                            }
+                            echo "<a href=\"usuario.php?f=m&u=".
+                                    $usuario['idUsuario']."\">Modificar</a>"
+                                    . " - <a href=\"usuario.php?f=e&u="
+                                    .$usuario['idUsuario']."\">Eliminar</a>"
+                                    . "</td></tr>";
+                        }
+                        ?>
+                    </tbody>
+                    </table>
+                    
+                    
+                    <?php } ?>
                 </div>
             </div>
             <footer>
@@ -142,7 +234,7 @@ if ($sesion->getPrivilegios() < $nivelAcceso) {
                 $('ul.sf-menu').sooperfish();
 <?php if (sizeof($msg) > 1) { ?>
                     alert("<?php echo $msg ?>");
-<?php } ?>
+<?php } mysql_close($db);?>
             });
         </script>
     </body>
